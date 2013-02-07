1d8e80fce68788a19b869dae0f6a4b2fc6397cb4
titanrain <titanmatter@gmail.com>
2013-02-07 12:26:14 +1100

    add info

--- Netest.py
+++ Netest.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# https://github.com/samuel/python-ping
 # http://wiki.worldoftanks.ru/%D0%98%D0%B3%D1%80%D0%BE%D0%B2%D1%8B%D0%B5_%D0%BA%D0%BB%D0%B0%D1%81%D1%82%D0%B5%D1%80%D1%8B
 # https://support.worldoftanks.ru/Knowledgebase/Article/View/362/18/pochemu-vysokijj-pingping-i-krsnja-lmpochk-laglg
 # http://rfc2.ru/792.rfc
